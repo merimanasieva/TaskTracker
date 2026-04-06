088e86ff36c9a3eb17585339c2d368cbcd3c5950
merimanasieva <merimanasieva2004@gmail.com>
2026-04-06 16:31:46 +0200

    Initial commit

---
+++ TaskItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskTrackerApp;
+
+public class TaskItem
+{
+    public string Title { get; set; } ="";
+    public string StartDate { get; set; } =";";
+    public string DueDate { get; set; } =";";
+    public bool IsCompleted { get; set; } 
+}
